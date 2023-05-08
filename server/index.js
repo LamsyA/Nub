f0fe54a72c22176d80f30d593698103cd49ea106
LamsyA <olamide.adetula.1744@gmail.com>
2023-05-08 19:26:11 +0100

    frontend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const db = require('./models')
-
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors())
 // routes
 
 const postRouter = require('./routes/Posts')
